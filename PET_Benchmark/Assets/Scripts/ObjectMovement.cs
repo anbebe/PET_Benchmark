0ae7df7c19fdb053ba11fa92d04148e4a546bfa7
Isabel Grauwelman <igrauwelman@uni-osnabrueck.de>
2023-01-16 15:33:09 +0100

    add object movement without player collision

--- PET_Benchmark/Assets/Scripts/ObjectMovement.cs
+++ PET_Benchmark/Assets/Scripts/ObjectMovement.cs
@@ -3,25 +3,63 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ObjectMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float destroyDelay;
     private Vector3 targetPosition;
+    private bool isCollisionObject;
     
-    void Start()
+    private void Start()
     {
-        targetPosition = new Vector3(transform.position.x * (-1), 1f, transform.position.z * (-1));
+        isCollisionObject = Random.value > .25; // random True or False generator, with True being more probable
+        
+        if (isCollisionObject)
+        {
+            targetPosition = new Vector3(transform.position.x * (-1), 1f, transform.position.z * (-1));
+        }
+        else // let object move without colliding with the player; target positions hard-coded --> TODO: easier?
+        {
+            switch (transform.position.z)
+            {
+                case < -3f when transform.position.x >= 0f:
+                    targetPosition = Random.value > 0.5 ? new Vector3(-4f, 1f, 0f) : new Vector3(4f, 1f, 4f);
+                    break;
+                case < -3f:
+                    targetPosition = Random.value > 0.5 ? new Vector3(4f, 1f, 0f) : new Vector3(-4f, 1f, 4f);
+                    break;
+                case > 3f when transform.position.x >= 0f:
+                    targetPosition = Random.value > 0.5 ? new Vector3(-4f, 1f, 0f) : new Vector3(4f, 1f, -4f);
+                    break;
+                case > 3f:
+                    targetPosition = Random.value > 0.5 ? new Vector3(4f, 1f, 0f) : new Vector3(-4f, 1f, -4f);
+                    break;
+                case < 0f when transform.position.x >= 0f:
+                    targetPosition = Random.value > 0.5 ? new Vector3(0f, 1f, 4f) : new Vector3(-4f, 1f, -4f);
+                    break;
+                case < 0f:
+                    targetPosition = Random.value > 0.5 ? new Vector3(0f, 1f, 4f) : new Vector3(4f, 1f, -4f);
+                    break;
+                case >= 0f when transform.position.x >= 0f:
+                    targetPosition = Random.value > 0.5 ? new Vector3(0f, 1f, -4f) : new Vector3(-4f, 1f, 4f);
+                    break;
+                case >= 0f:
+                    targetPosition = Random.value > 0.5 ? new Vector3(0f, 1f, -4f) : new Vector3(4f, 1f, 4f);
+                    break;
+            }
+        }
+        
         StartCoroutine(DestroyObject());
     }
     
-    void Update()
+    private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
     }
 
-    IEnumerator DestroyObject()
+    private IEnumerator DestroyObject()
     {
         yield return new WaitForSeconds(destroyDelay);
         
