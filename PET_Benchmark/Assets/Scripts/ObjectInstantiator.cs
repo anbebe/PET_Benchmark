c54be352ace94e216e9dd20a457f4201fafb5cc7
Isabel Grauwelman <igrauwelman@uni-osnabrueck.de>
2023-01-21 15:01:11 +0100

    controlled object instantiation
    immediate result, next: add 2 more instantiation patterns

--- PET_Benchmark/Assets/Scripts/ObjectInstantiator.cs
+++ PET_Benchmark/Assets/Scripts/ObjectInstantiator.cs
@@ -1,41 +1,98 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
+/// <summary>
+/// Instantiates objects the participant needs to avoid colliding with on a circle with radius 4 around the participant
+/// </summary>
 public class ObjectInstantiator : MonoBehaviour
 {
-    [SerializeField] private GameObject obj;
+    [SerializeField] private GameObject obj; // prefab
     [SerializeField] private float delay;
     [SerializeField] private float preparationDelay;
-    private bool spawnObjects;
-
-    public bool SpawnNewObjects
-    {
-        get => spawnObjects;
-        set => spawnObjects = value;
-    }
     
-    void Start()
+    [SerializeField] [Tooltip("y coordinate should always be 1; coordinates will be used to calculate the corresponding point on a circle around the player with radius 4")]  private List<Vector3> objPositions;
+
+    [SerializeField] [Tooltip("note: dictionaries cannot be serialized yet; add indices of objects that should not collide with the player but pass them")]  private List<int> passIndices;
+    [SerializeField] [Tooltip("add coordinates of the general target position, the corresponding point on the circle will be calculated")]  private List<Vector3> passTargetPos;
+    private Dictionary<int, Vector3> passObj = new Dictionary<int, Vector3>();
+    
+    private float xObj, zObj, mLine;
+
+    private GameObject instantiatedObj; // specific object that was instantiated
+
+    private void Start()
     {
-        spawnObjects = true;
+        CreatePassObjDictionary(passIndices, passTargetPos);
         StartCoroutine(SpawnObjects());
     }
 
-    IEnumerator SpawnObjects()
+    private IEnumerator SpawnObjects()
     {
         yield return new WaitForSeconds(preparationDelay);
-        
-        while (spawnObjects)
-        {
-            Vector2 randomPoint = Random.insideUnitCircle.normalized * 4f;
-            Vector3 pos = new Vector3(randomPoint.x, 1f, randomPoint.y);
-            Instantiate(obj, pos, Quaternion.identity, this.transform);
 
+        // for each entry in the list of general positions set in the editor
+        for (var i = 0; i < objPositions.Count; i++)
+        {
+            var dirPos = objPositions[i];
+            // calculate the corresponding point on the circle around the participant
+            Vector3 pos = CalculateCoordinates(dirPos);
+            
+            // instantiate object
+            instantiatedObj = Instantiate(obj, pos, Quaternion.identity, this.transform);
+            
+            // check if object should just pass the participant
+            foreach (var (ind, targetPos) in passObj)
+            {
+                if (ind == i) 
+                {
+                    // if yes, set collision bool to false and target position (calculated to be on the circle) as set in the editor
+                    instantiatedObj.GetComponent<ObjectMovement>().IsCollisionObject = false;
+                    instantiatedObj.GetComponent<ObjectMovement>().TargetPosition = CalculateCoordinates(targetPos);
+                    break;
+                }
+                // if not, set collision bool to true
+                instantiatedObj.GetComponent<ObjectMovement>().IsCollisionObject = true;
+                
+            }
+            
+            // wait before instantiating the next object
             yield return new WaitForSeconds(delay);
+        }
+    }
+
+    // takes the coordinates of the general point (direction) and calculates the coordinates of the corresponding point on the circle
+    // (calculates the intersection of the circle and a line that goes through the center (0,0,0) and the general point set in the editor)
+    private Vector3 CalculateCoordinates(Vector3 dirPos)
+    {
+        mLine = dirPos.z / dirPos.x;
+        xObj = Mathf.Sqrt(16 / (Mathf.Pow(mLine, 2f) + 1));
+        zObj = Mathf.Sqrt(16 - Mathf.Pow(xObj, 2));
+
+        // if coordinates set in the editor were negative, set the calculated to be negative as well
+        // (Mathf.Sqrt gives positive value of the square root as the result)
+        if (dirPos.x < 0)
+        {
+            xObj *= (-1);
+        }
+
+        if (dirPos.z < 0)
+        {
+            zObj *= (-1);
+        }
+        
+        return new Vector3(xObj, 1f, zObj);
+    }
+
+    // Unity cannot handle serialized dictionaries yet, so we need to work with 2 lists in the editor and create a dictionary from these entries
+    private void CreatePassObjDictionary(List<int> indices, List<Vector3> targets)
+    {
+        for (var i = 0; i < indices.Count; i++)
+        {
+            passObj[indices[i]] = targets[i];
         }
     }
 }
--- PET_Benchmark/Assets/Scripts/ObjectMovement.cs
+++ PET_Benchmark/Assets/Scripts/ObjectMovement.cs
@@ -1,56 +1,35 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
+/// <summary>
+/// Manages the object movement
+/// </summary>
 public class ObjectMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float destroyDelay;
     private Vector3 targetPosition;
-    private bool isCollisionObject;
+    public Vector3 TargetPosition
+    {
+        get => targetPosition;
+        set => targetPosition = value;
+    }
     
+    private bool isCollisionObject;
+    public bool IsCollisionObject
+    {
+        set => isCollisionObject = value;
+    }
+
     private void Start()
     {
-        isCollisionObject = Random.value > .25; // random True or False generator, with True being more probable
-        
         if (isCollisionObject)
         {
+            Debug.Log("obj is collider object");
             targetPosition = new Vector3(transform.position.x * (-1), 1f, transform.position.z * (-1));
         }
-        else // let object move without colliding with the player; target positions hard-coded --> TODO: easier?
-        {
-            switch (transform.position.z)
-            {
-                case < -3f when transform.position.x >= 0f:
-                    targetPosition = Random.value > 0.5 ? new Vector3(-4f, 1f, 0f) : new Vector3(4f, 1f, 4f);
-                    break;
-                case < -3f:
-                    targetPosition = Random.value > 0.5 ? new Vector3(4f, 1f, 0f) : new Vector3(-4f, 1f, 4f);
-                    break;
-                case > 3f when transform.position.x >= 0f:
-                    targetPosition = Random.value > 0.5 ? new Vector3(-4f, 1f, 0f) : new Vector3(4f, 1f, -4f);
-                    break;
-                case > 3f:
-                    targetPosition = Random.value > 0.5 ? new Vector3(4f, 1f, 0f) : new Vector3(-4f, 1f, -4f);
-                    break;
-                case < 0f when transform.position.x >= 0f:
-                    targetPosition = Random.value > 0.5 ? new Vector3(0f, 1f, 4f) : new Vector3(-4f, 1f, -4f);
-                    break;
-                case < 0f:
-                    targetPosition = Random.value > 0.5 ? new Vector3(0f, 1f, 4f) : new Vector3(4f, 1f, -4f);
-                    break;
-                case >= 0f when transform.position.x >= 0f:
-                    targetPosition = Random.value > 0.5 ? new Vector3(0f, 1f, -4f) : new Vector3(-4f, 1f, 4f);
-                    break;
-                case >= 0f:
-                    targetPosition = Random.value > 0.5 ? new Vector3(0f, 1f, -4f) : new Vector3(4f, 1f, 4f);
-                    break;
-            }
-        }
-        
+
         StartCoroutine(DestroyObject());
     }
     
